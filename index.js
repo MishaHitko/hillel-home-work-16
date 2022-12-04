b0d338e0ceb5e999b2e7913e0e175cfeab2d5e70
MishaHitko <mishagitko1997@gmail.com>
2022-12-04 15:34:07 +0300

    home work

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const table = document.createElement('table');
-document.body.prepend(table);
-let numbersCount = 1;
-for (let count = 1; count <= 10; count++) {
-    const td = document.createElement('td');
-    table.append(td);
-    for (let count1 = 1; count1 <= 10; count1++) {
-        const tr = document.createElement('tr');
-        td.append(tr);
-        tr.innerText = `${numbersCount}`;
-        numbersCount++;
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+const randomImg = getRandomIntInclusive(1, 9);
+const img = document.createElement('img');
+img.setAttribute('src', `img/${randomImg}.jpg`);
+document.body.append(img);
+
+
+
+
+//Вариант, если выводить картинки в рандомном порядке
+const boxImg = document.createElement('div');
+let arrayRandom = [];
+for (let count = 0; arrayRandom.length < 9; count++) {
+    const randomImg = getRandomIntInclusive(1, 9);
+    while (!arrayRandom.includes(randomImg)) {
+        arrayRandom.push(randomImg);
+        const img = document.createElement('img');
+        img.setAttribute('src', `img/${randomImg}.jpg`);
+        boxImg.append(img);
     }
 }
-
+document.body.append(boxImg);
 
